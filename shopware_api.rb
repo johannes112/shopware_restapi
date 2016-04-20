5541a5f337cfbf200271ad14385832c16dcc0be9
johannes112 <johannes.emmos@gmail.com>
2016-04-20 13:57:20 +0000

    implemented hash

--- shopware_api.rb
+++ shopware_api.rb
@@ -29,15 +29,17 @@ class ShopwareApi
   #customers
   def getCustomers() #get all customers
     options = { :digest_auth => @auth_digest }
-    response = self.class.get("/api/customers", options)
-    hash = response.parsed_response
-    if response.success?
-      response
+    response_data = self.class.get("/api/customers", options)
+    #p response_data
+    #to get json:
+    #response_json = response_data.parsed_response
+    #p "\n\n#{response_json}"
+    processHash(response_data)
+    if response_data.success?
+      response_data
     else
-      raise response.response
+      raise response_data.response_data
     end
-    puts "Keys:#{hash.keys}"
-    processHash(hash)
   end
 
   
@@ -53,15 +55,87 @@ class ShopwareApi
     
   end
   
-  def processHash(given_hash)
-    #all
+  def processHash(given_response_httpParty)
+    #get whole data of api
+    whole_response = given_response_httpParty.parsed_response
+    #get all keys
+    whole_keys = whole_response.keys
+    #save part of keys
+    data = whole_response.fetch("data")
+    data_id = data.fetch("id")
+    total = whole_response.fetch("total")
+    last_entity = total-1
+    #printAll(data, total)
+    p data[1]
+    #save entities of data
+    #p data[last_entity]
+
+    # p totaljson_response = JSON.parse(given_response_httpParty.body)
+    # p "json_response: #{json_response.class}"
+    # #json_response_customer_id= hash_response_httpParty["data"][1]
+    # json_response_customer = json_response.fetch("data")
+    # json_response_customer_id = json_response_customer.find { |h| h['id'] == '1' }
+    # json_response_customer_mail = json_response_customer.find { |h| h['mail'] == 'nils.wolfram@em-group.de' }
+    #p json_response_customer_id
+    #p json_response_customer_mail
+    #cu = json_response["data"].find{ |customer| customer['id']=='1'}['email']
+    #puts cu
+    
+    #puts "\ncustomers:#{hash_customers["groupKey"]}"
+    #puts "\nid:#{given_hash["data"].select {|customer| customer["id"] = 1}}"
     #given_hash.each_pair {|key,value| puts "#{key} = #{value}"}
-    #id
+    
+    # given_hash.each do |key, value|
+    #   case (key)
+    #   when 'data'
+    #     handle_some_key(value)
+    #   when 'total'
+    #     handle_other_key(value)
+    #   end
+    #end
     #filtered_hash = 
     #given_hash.each_pair {|key,value| puts key if value == 1} 
     #puts filtered_hash
-    converted_hash = JSON.parse(given_hash)
-    puts converted_hash "id" => "nils.wolfram@em-group.de"
+    #converted_hash = JSON.parse(given_hash)
+    #puts converted_hash
+    
+    # json_response = JSON.parse(given_response_httpParty.body)
+    # p "json_response: #{json_response.class}"
+    # #json_response_customer_id= hash_response_httpParty["data"][1]
+    # json_response_customer = json_response.fetch("data")
+    # json_response_customer_id = json_response_customer.find { |h| h['id'] == '1' }
+    # json_response_customer_mail = json_response_customer.find { |h| h['mail'] == 'nils.wolfram@em-group.de' }
+    #p json_response_customer_id
+    #p json_response_customer_mail
+    #cu = json_response["data"].find{ |customer| customer['id']=='1'}['email']
+    #puts cu
+    
+    #puts "\ncustomers:#{hash_customers["groupKey"]}"
+    #puts "\nid:#{given_hash["data"].select {|customer| customer["id"] = 1}}"
+    #given_hash.each_pair {|key,value| puts "#{key} = #{value}"}
+    
+    # given_hash.each do |key, value|
+    #   case (key)
+    #   when 'data'
+    #     handle_some_key(value)
+    #   when 'total'
+    #     handle_other_key(value)
+    #   end
+    #end
+    #filtered_hash = 
+    #given_hash.each_pair {|key,value| puts key if value == 1} 
+    #puts filtered_hash
+    #converted_hash = JSON.parse(given_hash)
+    #puts converted_hash
+  end
+
+  def printAll(data, last_element)  
+  counter = 0
+    while counter < last_element do
+      p ""
+      p data[counter]
+      counter += 1
+    end
   end
   
 end
